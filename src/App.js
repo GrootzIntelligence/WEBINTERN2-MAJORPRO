30c18272a99d19cf25ba40b804413bc96cc616e0
Sahil Wadhwa <sahil06.wadhwa@gmail.com>
2024-07-23 14:14:56 +0530

    initial_commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import VantaComponent from "./components/VantaComponent";
+
+function App() {
+  return (
+    <div className="App h-screen w-screen overflow-x-hidden flex flex-col">
+        <VantaComponent/> 
+    </div>
+     
+  );
+}
+
+export default App;
---
+++ src/components/Body.jsx
@@ -0,0 +1,10 @@
+function Body(){
+    return (
+        <div className="flex flex-col">
+            <div className="w-screen h-screen"></div>
+
+        </div>
+    )
+}
+
+export default Body;
