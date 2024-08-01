4f278817a949144d559370186c15e98a52c1e57e
Rishabh-Dev-Mishra <22bcc70149@cuchd.in>
2024-08-01 10:19:04 +0530

    Footer Updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}","./index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black': '#000',
+        'transparent': 'transparent',
+        'white': '#fff',
+        'gray': {
+          800: '#333',
+        },
+        'blue': {
+          600: '#1d4ed8',
+        }
+      },
+    },
   },
   plugins: [],
 }
