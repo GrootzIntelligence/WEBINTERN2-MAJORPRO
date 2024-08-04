3045218b5e8a2ad732c2eca69a3979b6b90abddc
Rishabh-Dev-Mishra <22bcc70149@cuchd.in>
2024-08-04 14:44:08 +0530

    Welcome and Contact part updated

--- src/components/Body_Welcome.jsx
+++ src/components/Body_Welcome.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 function Body_Welcome() {
   return (
     <div className="flex items-center justify-center h-full">
-      <h1 className="text-white text-4xl text-center text-bold">
-        Welcome <br />
-        to <br />
+      <h1 className="text-green-300 text-8xl text-center text-bold">
+        <h1 className="text-white text-8xl text-center text-bold">Welcome <br />
+        to <br /></h1>
+      
         Grootz Intelligence
       </h1>
     </div>
