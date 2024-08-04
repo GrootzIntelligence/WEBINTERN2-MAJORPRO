f39bdc56dd878be42b30bac3eb4ebb71cdacee6c
divyanshi kulshrestha <divyanshikulshrestha9@gmail.com>
2024-08-04 23:36:45 +0530

    responsive-card

--- src/components/Body_Welcome.jsx
+++ src/components/Body_Welcome.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 function Body_Welcome() {
   return (
     <div className="flex items-center justify-center h-full">
-      <h1 className="text-green-300 text-8xl text-center text-bold">
-        <h1 className="text-white text-8xl text-center text-bold">Welcome <br />
+      <h1 className="text-green-300 text-5xl text-center text-bold sm:text-8xl">
+        <h1 className="text-white text-center text-bold">Welcome <br />
         to <br /></h1>
       
         Grootz Intelligence
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,17 @@
 export default function Card({title, description, image}) {
     return(
-        <div className="flex w-full h-full">
+        <div className="flex w-full h-full flex-wrap-reverse">
 
 
-        <div className="flex-1 p-4">
-          <h2 className="text-2xl font-bold mb-2">{title}</h2>
+        <div className="flex-1 p-4 min-w-72">
+          <h2 className="text-2xl font-bold mb-2 underline underline-offset-8">{title}</h2>
           <p className="text-lg">
          {description}
           </p>
         </div>
 
 
-        <div className="flex-1 p-4">
+        <div className="flex-1 p-4  min-w-72">
           <img src={image} alt="About Us" className="w-full h-auto object-cover" />
         </div>
       </div>
