ac586c0cdcd1794913396b760f193bbfc1a8fe91
Abhishek goyal <abhishekgoyal1311@gmail.com>
2024-08-16 21:56:37 +0530

    fade-in-fade-out-animation

--- src/components/VantaComponent.js
+++ src/components/VantaComponent.js
@@ -1,19 +1,25 @@
-import React, { useState, useEffect, useRef } from 'react'
-import Navbar from './Navbar'
-import Footer from './Footer'
-import Body from './Body'
+import React from 'react';
+import { motion } from 'framer-motion';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Body from './Body';
 
-const VantaComponent = (props) => {
-
-  return (    
-      <div className='w-screen overflow-x-hidden flex flex-col'>
-          <div className='flex flex-col object-contain'>
-            <Navbar/>
-            <Body/>
-            <Footer/>
-        </div>
+const VantaComponent = () => {
+  return (
+    <motion.div
+      className="w-screen overflow-x-hidden flex flex-col"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1 }}
+    >
+      <div className='flex flex-col object-contain'>
+        <Navbar />
+        <Body />
+        <Footer />
       </div>
-      )
-}
+    </motion.div>
+  );
+};
 
 export default VantaComponent;
